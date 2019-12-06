56a7ea2d58f412ccea12cda13916fbc1cbf71887
Lalith Kumar <lalith889@gmail.com>
2019-12-06 15:30:01 +0530

    test commit

--- index.js
+++ index.js
@@ -3,12 +3,26 @@
 var fs = require('fs');
 var path = require('path');
 
-exports.get = function(event, context, callback) {
+var express = require('express');
+
+var app = express();
+
+app.get('/test', (req, res) => {
+  res.send({ value: true });
+});
+
+var listen = app.listen(process.env.port || 80);
+if (listen) {
+  console.log('connected ******************');
+} else {
+  console.log('Failed ******************')
+}
+exports.get = function (event, context, callback) {
   var contents = fs.readFileSync(`public${path.sep}index.html`);
   var result = {
     statusCode: 200,
     body: contents.toString(),
-    headers: {'content-type': 'text/html'}
+    headers: { 'content-type': 'text/html' }
   };
   console.log('We can do');
   callback(null, result);
